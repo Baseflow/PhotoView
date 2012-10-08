5eebcb283fc6d247ae8925fb39a435c23b6a0c32
Chris Banes <chris@senab.co.uk>
2012-10-08 13:26:58 +0100

    Expand Sample app. Fixes #2

--- sample/src/uk/co/senab/photoview/sample/MainActivity.java
+++ sample/src/uk/co/senab/photoview/sample/MainActivity.java
@@ -17,26 +17,81 @@ package uk.co.senab.photoview.sample;
 
 import uk.co.senab.photoview.PhotoView;
 import android.app.Activity;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
+
+	static final String PHOTO_TAP_TOAST_STRING = "Photo Tap! X: %.2f %% Y:%.2f %%";
+
 	private PhotoView mPhotoView;
+	private TextView mCurrMatrixTv;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mPhotoView = (PhotoView) findViewById(R.id.pv_photo);
-        
-        Drawable bitmap = getResources().getDrawable(R.drawable.wallpaper);
-        mPhotoView.setImageDrawable(bitmap);
-        mPhotoView.setZoomable(true);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    
+		mPhotoView = (PhotoView) findViewById(R.id.pv_photo);
+		mCurrMatrixTv = (TextView) findViewById(R.id.tv_current_matrix);
 
-    
+		Drawable bitmap = getResources().getDrawable(R.drawable.wallpaper);
+		mPhotoView.setImageDrawable(bitmap);
+		mPhotoView.setZoomable(true);
+
+		mPhotoView.setOnMatrixChangeListener(new MatrixChangeListener());
+		mPhotoView.setOnPhotoTapListener(new PhotoTapListener());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		MenuItem zoomToggle = menu.findItem(R.id.menu_zoom_toggle);
+		zoomToggle.setTitle(mPhotoView.canZoom() ? R.string.menu_zoom_disable : R.string.menu_zoom_enable);
+
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_zoom_toggle:
+				mPhotoView.setZoomable(!mPhotoView.canZoom());
+				return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	private class PhotoTapListener implements PhotoView.OnPhotoTapListener {
+
+		@Override
+		public void onPhotoTap(View view, float x, float y) {
+			float xPercentage = x * 100f;
+			float yPercentage = y * 100f;
+
+			Toast.makeText(MainActivity.this, String.format(PHOTO_TAP_TOAST_STRING, xPercentage, yPercentage),
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	private class MatrixChangeListener implements PhotoView.OnMatrixChangedListener {
+
+		@Override
+		public void onMatrixChanged(RectF rect) {
+			mCurrMatrixTv.setText(rect.toString());
+		}
+	}
+
 }
