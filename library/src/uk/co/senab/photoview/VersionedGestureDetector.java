9e9617e8d897d40425820bcf41b13002f097b197
Chris Banes <chris@senab.co.uk>
2012-12-03 17:41:06 +0000

    Fix #14

--- library/src/uk/co/senab/photoview/VersionedGestureDetector.java
+++ library/src/uk/co/senab/photoview/VersionedGestureDetector.java
@@ -16,11 +16,13 @@ package uk.co.senab.photoview;
  * limitations under the License.
  *******************************************************************************/
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.FloatMath;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
+import android.view.ScaleGestureDetector.OnScaleGestureListener;
 import android.view.VelocityTracker;
 import android.view.ViewConfiguration;
 
@@ -31,9 +33,10 @@ public abstract class VersionedGestureDetector {
 	public static VersionedGestureDetector newInstance(Context context, OnGestureListener listener) {
 		final int sdkVersion = Build.VERSION.SDK_INT;
 		VersionedGestureDetector detector = null;
-		if (sdkVersion < Build.VERSION_CODES.ECLAIR) {
+
+		if (sdkVersion < 5) {
 			detector = new CupcakeDetector(context);
-		} else if (sdkVersion < Build.VERSION_CODES.FROYO) {
+		} else if (sdkVersion < 8) {
 			detector = new EclairDetector(context);
 		} else {
 			detector = new FroyoDetector(context);
@@ -164,6 +167,7 @@ public abstract class VersionedGestureDetector {
 		}
 	}
 
+	@TargetApi(5)
 	private static class EclairDetector extends CupcakeDetector {
 		private static final int INVALID_POINTER_ID = -1;
 		private int mActivePointerId = INVALID_POINTER_ID;
@@ -221,12 +225,35 @@ public abstract class VersionedGestureDetector {
 		}
 	}
 
-	private static class FroyoDetector extends EclairDetector implements ScaleGestureDetector.OnScaleGestureListener {
-		private ScaleGestureDetector mDetector;
+	@TargetApi(8)
+	private static class FroyoDetector extends EclairDetector {
+
+		private final ScaleGestureDetector mDetector;
+
+		// Needs to be an inner class so that we don't hit
+		// VerifyError's on API 4.
+		private final OnScaleGestureListener mScaleListener = new OnScaleGestureListener() {
+
+			@Override
+			public boolean onScale(ScaleGestureDetector detector) {
+				mListener.onScale(detector.getScaleFactor(), detector.getFocusX(), detector.getFocusY());
+				return true;
+			}
+
+			@Override
+			public boolean onScaleBegin(ScaleGestureDetector detector) {
+				return true;
+			}
+
+			@Override
+			public void onScaleEnd(ScaleGestureDetector detector) {
+				// NO-OP
+			}
+		};
 
 		public FroyoDetector(Context context) {
 			super(context);
-			mDetector = new ScaleGestureDetector(context, this);
+			mDetector = new ScaleGestureDetector(context, mScaleListener);
 		}
 
 		@Override
@@ -235,25 +262,10 @@ public abstract class VersionedGestureDetector {
 		}
 
 		@Override
-		public boolean onScale(ScaleGestureDetector detector) {
-			mListener.onScale(detector.getScaleFactor(), detector.getFocusX(), detector.getFocusY());
-			return true;
-		}
-
-		@Override
 		public boolean onTouchEvent(MotionEvent ev) {
 			mDetector.onTouchEvent(ev);
 			return super.onTouchEvent(ev);
 		}
 
-		@Override
-		public boolean onScaleBegin(ScaleGestureDetector detector) {
-			return true;
-		}
-
-		@Override
-		public void onScaleEnd(ScaleGestureDetector detector) {
-			// NO-OP
-		}
 	}
 }
