888adf23090b367d63f6febe39d2a0390b241f78
Steve Lhomme <slhomme@levelupstudio.com>
2013-03-14 12:12:25 +0100

    @Override is always a good indication in the code

--- library/src/uk/co/senab/photoview/PhotoViewAttacher.java
+++ library/src/uk/co/senab/photoview/PhotoViewAttacher.java
@@ -354,6 +354,7 @@ public class PhotoViewAttacher implements IPhotoView, View.OnTouchListener, Vers
 		}
 	}
 
+	@Override
 	public final boolean onSingleTapConfirmed(MotionEvent e) {
 		ImageView imageView = getImageView();
 
