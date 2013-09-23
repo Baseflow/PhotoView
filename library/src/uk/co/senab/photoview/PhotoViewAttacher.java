2a6a34a3ba0bc1eadd6ccb08df6abf7c629c72d9
mareksebera <marek.sebera@gmail.com>
2013-09-23 21:22:26 +0200

    Fixed compatibility with calls FloatMath

--- library/src/uk/co/senab/photoview/PhotoViewAttacher.java
+++ library/src/uk/co/senab/photoview/PhotoViewAttacher.java
@@ -303,7 +303,7 @@ public class PhotoViewAttacher implements IPhotoView, View.OnTouchListener,
 
     @Override
     public final float getScale() {
-        return FloatMath.sqrt(FloatMath.pow(getValue(mSuppMatrix, Matrix.MSCALE_X), 2) + FloatMath.pow(getValue(mSuppMatrix, Matrix.MSKEW_Y), 2));
+        return FloatMath.sqrt((float) Math.pow(getValue(mSuppMatrix, Matrix.MSCALE_X), 2) + (float) Math.pow(getValue(mSuppMatrix, Matrix.MSKEW_Y), 2));
     }
 
     @Override
