f2b4114d045fcf5e3c3aef3d20459830cc7e9fbe
mareksebera <marek.sebera@gmail.com>
2014-01-16 14:19:31 +0100

    NaN/infinite check on request, Closing #126

--- library/src/uk/co/senab/photoview/gestures/FroyoGestureDetector.java
+++ library/src/uk/co/senab/photoview/gestures/FroyoGestureDetector.java
@@ -31,7 +31,12 @@ public class FroyoGestureDetector extends EclairGestureDetector {
 
             @Override
             public boolean onScale(ScaleGestureDetector detector) {
-                mListener.onScale(detector.getScaleFactor(),
+                float scaleFactor = detector.getScaleFactor();
+
+                if (Float.isNaN(scaleFactor) || Float.isInfinite(scaleFactor))
+                    return false;
+
+                mListener.onScale(scaleFactor,
                         detector.getFocusX(), detector.getFocusY());
                 return true;
             }
