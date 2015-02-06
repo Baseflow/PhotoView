804ed55d02ca01ece66a232a68b215d29209a8d6
Bryan Herbst <tanis.7x@gmail.com>
2015-02-06 12:24:12 -0600

    Fix for regression on issue #72.
    If the PhotoView gesture detector handles scaling or dragging the PhotoView, we will continue to block the parent view from receiving onTouch events. If the gesture detector did not scale or drag the PhotoView at all, we allow the parent to properly receive all touch events.
    
    Tested on 4.x and 5.x in an Activity with both a NavigationDrawer and a ViewPager. Both components work as designed along with scaling/panning a PhotoView.

--- library/src/main/java/uk/co/senab/photoview/gestures/CupcakeGestureDetector.java
+++ library/src/main/java/uk/co/senab/photoview/gestures/CupcakeGestureDetector.java
@@ -58,6 +58,10 @@ public class CupcakeGestureDetector implements GestureDetector {
         return false;
     }
 
+    public boolean isDragging() {
+        return mIsDragging;
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         switch (ev.getAction()) {
