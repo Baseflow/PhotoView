2c292fd73615ce77f9004e9cdf6bba276a774c29
Jonathan Zanella <jonathan.zanella@spartalabs.com.br>
2015-07-08 17:37:54 -0300

    Adding CropTypeImage and initZoomAtMax to PhotoView attrs

--- library/src/main/java/uk/co/senab/photoview/PhotoView.java
+++ library/src/main/java/uk/co/senab/photoview/PhotoView.java
@@ -16,6 +16,7 @@
 package uk.co.senab.photoview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.RectF;
@@ -31,9 +32,55 @@ import uk.co.senab.photoview.PhotoViewAttacher.OnViewTapListener;
 
 public class PhotoView extends ImageView implements IPhotoView {
 
+    public enum CustomScaleType {
+        MATRIX,
+        FIT_XY,
+        FIT_START,
+        FIT_CENTER,
+        FIT_END,
+        CENTER,
+        CENTER_CROP,
+        CENTER_INSIDE,
+
+        //Custom
+        TOP_LEFT_CROP,
+        TOP_CENTER_CROP,
+        TOP_RIGHT_CROP,
+        BOTTOM_LEFT_CROP,
+        BOTTOM_CENTER_CROP,
+        BOTTOM_RIGHT_CROP,
+        CENTER_LEFT_CROP,
+        CENTER_RIGHT_CROP;
+
+	    public ScaleType toScaleType() {
+		    switch (this) {
+			    case FIT_XY:
+				    return ScaleType.FIT_XY;
+			    case FIT_START:
+				    return ScaleType.FIT_START;
+			    case FIT_CENTER:
+				    return ScaleType.FIT_CENTER;
+			    case FIT_END:
+				    return ScaleType.FIT_END;
+			    case CENTER:
+				    return ScaleType.CENTER;
+			    case CENTER_CROP:
+				    return ScaleType.CENTER_CROP;
+			    case CENTER_INSIDE:
+				    return ScaleType.CENTER_INSIDE;
+			    case MATRIX:
+			    default:
+				    return ScaleType.MATRIX;
+		    }
+	    }
+    }
+
+    private CustomScaleType customScaleType;
+	private boolean initZoomAtMax = false;
+
     private PhotoViewAttacher mAttacher;
 
-    private ScaleType mPendingScaleType;
+    private CustomScaleType mPendingScaleType;
 
     public PhotoView(Context context) {
         this(context, null);
@@ -46,21 +93,87 @@ public class PhotoView extends ImageView implements IPhotoView {
     public PhotoView(Context context, AttributeSet attr, int defStyle) {
         super(context, attr, defStyle);
         super.setScaleType(ScaleType.MATRIX);
-        init();
+        init(attr);
     }
 
-    protected void init() {
+    protected void init(AttributeSet attr) {
         if (null == mAttacher || null == mAttacher.getImageView()) {
             mAttacher = new PhotoViewAttacher(this);
         }
 
         if (null != mPendingScaleType) {
-            setScaleType(mPendingScaleType);
+	        setCustomScaleType(mPendingScaleType);
             mPendingScaleType = null;
         }
+
+	    if(attr != null)
+		    initCustomScaleType(attr);
     }
 
-    /**
+	private void initCustomScaleType(AttributeSet attr) {
+		TypedArray a = getContext().getTheme().obtainStyledAttributes(attr, R.styleable.PhotoView, 0, 0);
+
+		initZoomAtMax = a.getBoolean(R.styleable.PhotoView_initZoomAtMax, false);
+
+		switch (a.getInt(R.styleable.PhotoView_scaleType, 0)) {
+			case 100:
+				customScaleType = CustomScaleType.TOP_LEFT_CROP;
+				break;
+			case 101:
+				customScaleType = CustomScaleType.TOP_CENTER_CROP;
+				break;
+			case 102:
+				customScaleType = CustomScaleType.TOP_RIGHT_CROP;
+				break;
+			case 103:
+				customScaleType = CustomScaleType.BOTTOM_LEFT_CROP;
+				break;
+			case 104:
+				customScaleType = CustomScaleType.BOTTOM_CENTER_CROP;
+				break;
+			case 105:
+				customScaleType = CustomScaleType.BOTTOM_RIGHT_CROP;
+				break;
+			case 106:
+				customScaleType = CustomScaleType.CENTER_LEFT_CROP;
+				break;
+			case 107:
+				customScaleType = CustomScaleType.CENTER_RIGHT_CROP;
+				break;
+			default: {
+				switch (getScaleType()) {
+					case CENTER:
+						customScaleType = CustomScaleType.CENTER;
+						break;
+					case CENTER_CROP:
+						customScaleType = CustomScaleType.CENTER_CROP;
+						break;
+					case CENTER_INSIDE:
+						customScaleType = CustomScaleType.CENTER_INSIDE;
+						break;
+					case FIT_CENTER:
+						customScaleType = CustomScaleType.FIT_CENTER;
+						break;
+					case FIT_END:
+						customScaleType = CustomScaleType.FIT_END;
+						break;
+					case FIT_START:
+						customScaleType = CustomScaleType.FIT_START;
+						break;
+					case FIT_XY:
+						customScaleType = CustomScaleType.FIT_XY;
+						break;
+					case MATRIX:
+						customScaleType = CustomScaleType.MATRIX;
+						break;
+				}
+			}
+		}
+
+		mAttacher.setCustomScaleType(customScaleType);
+	}
+
+	/**
      * @deprecated use {@link #setRotationTo(float)}
      */
     @Override
@@ -140,6 +253,14 @@ public class PhotoView extends ImageView implements IPhotoView {
     public ScaleType getScaleType() {
         return mAttacher.getScaleType();
     }
+
+	public CustomScaleType getCustomScaleType() {
+		return customScaleType;
+	}
+
+	public boolean initZoomAtMax() {
+		return initZoomAtMax;
+	}
 
     @Override
     public void setAllowParentInterceptOnEdge(boolean allow) {
@@ -259,9 +380,42 @@ public class PhotoView extends ImageView implements IPhotoView {
         if (null != mAttacher) {
             mAttacher.setScaleType(scaleType);
         } else {
-            mPendingScaleType = scaleType;
+	        switch (scaleType) {
+		        case CENTER:
+			        mPendingScaleType = CustomScaleType.CENTER;
+			        break;
+		        case CENTER_CROP:
+			        mPendingScaleType = CustomScaleType.CENTER_CROP;
+			        break;
+		        case CENTER_INSIDE:
+			        mPendingScaleType = CustomScaleType.CENTER_INSIDE;
+			        break;
+		        case FIT_CENTER:
+			        mPendingScaleType = CustomScaleType.FIT_CENTER;
+			        break;
+		        case FIT_END:
+			        mPendingScaleType = CustomScaleType.FIT_END;
+			        break;
+		        case FIT_START:
+			        mPendingScaleType = CustomScaleType.FIT_START;
+			        break;
+		        case FIT_XY:
+			        mPendingScaleType = CustomScaleType.FIT_XY;
+			        break;
+		        case MATRIX:
+			        mPendingScaleType = CustomScaleType.MATRIX;
+			        break;
+	        }
         }
     }
+
+	public void setCustomScaleType(CustomScaleType scaleType) {
+		if (null != mAttacher) {
+			mAttacher.setCustomScaleType(scaleType);
+		} else {
+			mPendingScaleType = scaleType;
+		}
+	}
 
     @Override
     public void setZoomable(boolean zoomable) {
@@ -301,7 +455,7 @@ public class PhotoView extends ImageView implements IPhotoView {
 
     @Override
     protected void onAttachedToWindow() {
-        init();
+        init(null);
         super.onAttachedToWindow();
     }
 }
---
+++ sample/src/main/java/uk/co/senab/photoview/sample/CropTypeActivity.java
@@ -0,0 +1,16 @@
+package uk.co.senab.photoview.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jonathan on 08/07/15.
+ * Copyright (c) 2015 Sparta Labs. All rights reserved.
+ */
+public class CropTypeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crop_type);
+	}
+}
