3f3bf196d8da3d84b5a3e21b104c73fda09c1dd2
John Carlson <jawnnypoo@gmail.com>
2015-09-17 10:19:27 -0500

    Correct some documentation so that maven will not complain.

--- library/src/main/java/uk/co/senab/photoview/PhotoViewAttacher.java
+++ library/src/main/java/uk/co/senab/photoview/PhotoViewAttacher.java
@@ -962,7 +962,7 @@ public class PhotoViewAttacher implements IPhotoView, View.OnTouchListener,
         /**
          * Callback for when the scale changes
          *
-         * @param scaleFactor the scale factor (<1 for zoom out, >1 for zoom in)
+         * @param scaleFactor the scale factor (less than 1 for zoom out, greater than 1 for zoom in)
          * @param focusX      focal point X position
          * @param focusY      focal point Y position
          */
