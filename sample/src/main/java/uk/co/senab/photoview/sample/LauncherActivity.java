2b35b44740b9ed1cfc0beaf7df4ffa9647055aab
Jawnnypoo <jawnnypoo@gmail.com>
2016-03-17 19:57:50 -0500

    Fix up the sample a bit and bump the version preping for release

--- sample/src/main/java/uk/co/senab/photoview/sample/LauncherActivity.java
+++ sample/src/main/java/uk/co/senab/photoview/sample/LauncherActivity.java
@@ -15,44 +15,90 @@
  *******************************************************************************/
 package uk.co.senab.photoview.sample;
 
-import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class LauncherActivity extends ListActivity {
+public class LauncherActivity extends AppCompatActivity {
 
     public static final String[] options = {"Simple Sample", "ViewPager Sample", "Rotation Sample", "Picasso Sample"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, options));
+        setContentView(R.layout.activity_launcher);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new ItemAdapter());
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        Class c;
 
-        switch (position) {
-            default:
-            case 0:
-                c = SimpleSampleActivity.class;
-                break;
-            case 1:
-                c = ViewPagerActivity.class;
-                break;
-            case 2:
-                c = RotationSampleActivity.class;
-                break;
-            case 3:
-                c = PicassoSampleActivity.class;
-                break;
+    private static class ItemAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return ItemViewHolder.newInstance(parent);
         }
 
-        startActivity(new Intent(this, c));
+        @Override
+        public void onBindViewHolder(final ItemViewHolder holder, final int position) {
+            holder.bind(options[position]);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Class c;
+
+                    switch (position) {
+                        default:
+                        case 0:
+                            c = SimpleSampleActivity.class;
+                            break;
+                        case 1:
+                            c = ViewPagerActivity.class;
+                            break;
+                        case 2:
+                            c = RotationSampleActivity.class;
+                            break;
+                        case 3:
+                            c = PicassoSampleActivity.class;
+                            break;
+                    }
+
+                    Context context = holder.itemView.getContext();
+                    context.startActivity(new Intent(context, c));
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return options.length;
+        }
     }
 
+    private static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        public static ItemViewHolder newInstance(ViewGroup parent) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list_item, parent, false);
+            return new ItemViewHolder(view);
+        }
+
+        public TextView mTextTitle;
+
+        public ItemViewHolder(View view) {
+            super(view);
+            mTextTitle = (TextView) view.findViewById(R.id.title);
+        }
+
+        private void bind(String title) {
+            mTextTitle.setText(title);
+        }
+    }
 }
