7e1b336e41e21bebbc189b76a10f8d6c5aa6a4e0
Atif Niyaz <atifniyaz11@gmail.com>
2017-05-27 12:41:29 -0700

    Added some documentation regarding drag

--- photoview/src/main/java/com/github/chrisbanes/photoview/OnViewDragListener.java
+++ photoview/src/main/java/com/github/chrisbanes/photoview/OnViewDragListener.java
@@ -9,8 +9,8 @@ public interface OnViewDragListener {
      * Callback for when the photo is experiencing a drag event. This cannot be invoked when the
      * user is scaling.
      *
-     * @param dx
-     * @param dy
+     * @param dx The change of the coordinates in the x-direction
+     * @param dy The change of the coordinates in the y-direction
      */
     void onDrag(float dx, float dy);
 }
