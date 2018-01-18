14660f8a92cab97020c12e9da87d244349f2b2c0
Vladislav.Nikolaev <vladislav.nikolaev@life.com.by>
2018-01-18 15:48:03 +0300

    disable medium scale

--- photoview/src/main/java/com/github/chrisbanes/photoview/PhotoViewAttacher.java
+++ photoview/src/main/java/com/github/chrisbanes/photoview/PhotoViewAttacher.java
@@ -236,9 +236,8 @@ public class PhotoViewAttacher implements View.OnTouchListener,
                     float x = ev.getX();
                     float y = ev.getY();
 
-                    if (scale < getMediumScale()) {
-                        setScale(getMediumScale(), x, y, true);
-                    } else if (scale >= getMediumScale() && scale < getMaximumScale()) {
+
+                    if (scale < getMaximumScale()) {
                         setScale(getMaximumScale(), x, y, true);
                     } else {
                         setScale(getMinimumScale(), x, y, true);
