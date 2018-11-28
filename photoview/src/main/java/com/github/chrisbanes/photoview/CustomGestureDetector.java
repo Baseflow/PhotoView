a84eba1ba06539790c0ee5e77fba599eeea66c77
xd720p <xd720p@gmail.com>
2018-11-28 14:42:39 +0300

    -Fixed flipping image on quickscaling when ScaleGestureDetector returns scale factor below zero

--- photoview/src/main/java/com/github/chrisbanes/photoview/CustomGestureDetector.java
+++ photoview/src/main/java/com/github/chrisbanes/photoview/CustomGestureDetector.java
@@ -55,9 +55,10 @@ class CustomGestureDetector {
 
                 if (Float.isNaN(scaleFactor) || Float.isInfinite(scaleFactor))
                     return false;
-
-                mListener.onScale(scaleFactor,
-                        detector.getFocusX(), detector.getFocusY());
+                if (scaleFactor >= 0) {
+                    mListener.onScale(scaleFactor,
+                            detector.getFocusX(), detector.getFocusY());
+                }
                 return true;
             }
 
