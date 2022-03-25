0f7b11d5543ffb37172f2998f16f42f9f0d8107a
BuYishi <bu-ish@qq.com>
2022-03-25 15:34:38 +0800

    activity_animation_sample.xml

---
+++ sample/src/main/java/com/github/chrisbanes/photoview/sample/AnimationSampleActivity.java
@@ -0,0 +1,14 @@
+package com.github.chrisbanes.photoview.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimationSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation_sample);
+    }
+}
